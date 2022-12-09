8e3b9a5d057044dbd4fdb2df48fefc20d1b26c1d
Maite <maicapref@gmail.com>
2022-12-09 08:49:23 -0300

    Estudos sobre microsservicos

---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/dto/ItemDoPedidoDto.java
@@ -0,0 +1,15 @@
+package br.com.alurafood.pedidos.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ItemDoPedidoDto {
+
+    private Long id;
+    private Integer quantidade;
+    private String descricao;
+}
